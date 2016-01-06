6c01f1b58527dd00ff0618a61d6ca081fb15ac55
wkaspryk <wkaspryk@pnimedia.com>
2016-01-06 15:48:56 -0800

    optimized code

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 Window.onload = function(event){
 
     console.log('loading..');
-    var imageLoader = document.getElementById('img_capture');
+    var imageLoader = document.getElementById('img_capture');    
     imageLoader.addEventListener('change', handleImage, false);
+
     var canvas = document.getElementById('img_canvas');
     var ctx = canvas.getContext('2d');
 
 
     function handleImage(e){
+
         var reader = new FileReader();
         reader.onload = function(event){
             var img = new Image();
@@ -16,6 +18,14 @@ Window.onload = function(event){
                 canvas.width = img.width;
                 canvas.height = img.height;
                 ctx.drawImage(img,0,0);
+
+                //landscape
+                if(img.width>img.height){
+                    $(canvas).removeClass('portrait').addClass('landscape');
+                }else{
+                    $(canvas).removeClass('landscape').addClass('portrait');
+                }
+
             }
             img.src = event.target.result;
         }
